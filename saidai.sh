bc3fe180d89b81e6b1335f98dda8877224a33e1c
a2025kt <a2025kt@aiit.ac.jp>
2020-07-05 16:22:58 +0900

    Create saidai.sh

---
+++ saidai.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#最大公約数を出力するシェルスクリプトです
+
+for i in `seq 100`
+do
+	let j=$1
+	let k=$2
+	let l=$j*$i
+	let m=$k*$i
+	array1+=($l)
+	array2+=($m)
+done
+
+#動作確認用(運用時コメントアウト)
+# echo ${array1[@]}
+# echo ${array2[@]}
+
+
+#ここから、2値を比較して同値を新しい配列"array3"に格納する
+for p in ${array1[@]}; do
+	for q in ${array2[@]}; do
+		if [ "${p}" = "${q}" ]; then
+			array3+=(${p})
+		fi
+	done
+done
+
+#ここから、配列に入った値から最小のもの(=最小公倍数)を出力する
+
+echo ${array3[0]}
